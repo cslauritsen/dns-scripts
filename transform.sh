0d7875309c8785b4056c23c81c87fd26195d7629
Chad Lauritsen <csl4jc@gmail.com>
2016-12-07 14:12:22 -0500

    last updated record

--- transform.sh
+++ transform.sh
@@ -1,2 +1,5 @@
-cat *.template | sed "s/%SERIAL%/$(date +%s)/" | sed "s
-/%HOMEIP%/$(echo $SSH_CONNECTION | awk '{print $1}')/"
+cat *.template \
+    | sed "s/%SERIAL%/$(date +%s)/" \
+    | sed "s/%HOMEIP%/$(echo $SSH_CONNECTION | awk '{print $1}')/" \
+    | sed "s/%LUPD%/$(date)/"
+    
